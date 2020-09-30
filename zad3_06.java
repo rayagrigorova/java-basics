e46347330f1ae8b9bd78be1403d23a8c4f64caa0
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-09-30 11:42:03 +0300

    Add files via upload

--- zad3_06.java
+++ zad3_06.java
@@ -12,13 +12,17 @@ public class zad3_06 {
 	   	 int daysAbsent;
 	   	 boolean exempt = false;
 	   	 Scanner reader = new Scanner(System.in);
+	   	 
 	   	 System.out.println("This program will determine if you can get out of the final exam.");
 	   	 System.out.println("Please answer the following questions.");
+	   	 
 	   	 System.out.println("What is your average in the class?");
 	   	 average = reader.nextDouble();
+	   	 
 	   	 System.out.println("How class lectures have you missed?");
 	   	 daysAbsent = reader.nextInt();
-	   	 if ((average >= 90 && daysAbsent <= 3) || (average >= 80 && daysAbsent <= 0)) {
+	   	 
+	   	 if ((average >= 90 && daysAbsent <= 3) || (average >= 80 && daysAbsent == 0)) {
 	   		System.out.println("Congratulations! You are exempt from the final exam.");
 	   	 }
 	   	 else
